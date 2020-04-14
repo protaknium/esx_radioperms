e70a2fadd0b32bd52c76c1dc985120b4051d73ca
Beebey <ilanbeebey@gmail.com>
2020-04-14 13:28:50 +0200

    2 loops -> 1 loop

--- client.lua
+++ client.lua
@@ -3,27 +3,18 @@ PlayerData = {}
 local done = false
 
 ESX = nil
+
  Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
         Citizen.Wait(0)
     end
-    while ESX.GetPlayerData().job == nil do
-        Citizen.Wait(10)
-    end
     PlayerData = ESX.GetPlayerData()
-    done = true
-end)
---==============ESX^^^^^^No TOUCHING!================---
-
- Citizen.CreateThread(function() 
-    while not done do
+    while PlayerData.job == nil do
         Citizen.Wait(10)
     end
     if PlayerData.job.name == 'police' then 
-	exports["rp-radio"]:GivePlayerAccessToFrequencies(1, 2, 3, 4, 5)    
-    done = true
+	    exports["rp-radio"]:GivePlayerAccessToFrequencies(1, 2, 3, 4, 5)    
     end
 end)
-
 
